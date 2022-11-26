4781982737ce297a79f71737445a62390e4e5c7d
Iván Ávalos <avalos@disroot.org>
2022-11-25 22:29:55 -0600

    Se añaden encabezados de licencia

--- compilador/parse/base.py
+++ compilador/parse/base.py
@@ -1,3 +1,19 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (C) 2022  Iván Alejandro Ávalos Díaz <avalos@disroot.org>
+#                     Edgar Alexis López Martínez <edgarmlmp@gmail.com>
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 from enum import Enum
 from tabla import LexToken, TablaLex, tokens
 from arbol import Arbol, Nodo
@@ -65,6 +81,6 @@ class BaseParser:
         '''Returns a syntax error if cond is false and void
         otherwise.
 
-        '''
+        '''<
         if not cond:
             return Error(msg = msg, numlinea = numlinea)
